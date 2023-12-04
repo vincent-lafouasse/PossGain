b6fa07c3d4dbf49669a55129651eb8ce247ecdee
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-04 22:02:37 +0100

    introducing Posscillator

---
+++ Plugins/Posscillator/Source/PluginEditor.h
@@ -0,0 +1,34 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <shared_plugin_helpers/shared_plugin_helpers.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+ */
+class PosscilatorAudioProcessorEditor : public juce::AudioProcessorEditor
+{
+   public:
+    PosscilatorAudioProcessorEditor(PosscilatorAudioProcessor&);
+    ~PosscilatorAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint(juce::Graphics&) override;
+    void resized() override;
+
+   private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PosscilatorAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(
+        PosscilatorAudioProcessorEditor)
+};
