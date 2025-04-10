4339cb22d409faa184e688d5a908c6ef6e9c31ae
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-10 16:39:41 +0200

    remove dead include

--- PossGain/Source/PossGain.hpp
+++ PossGain/Source/PossGain.hpp
@@ -9,7 +9,6 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "StereoSignalSmoother.tpp"
 
 //==============================================================================
 /**
