24adac2daf6b34300fbc3c5ef2730a5a9992efbe
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-15 19:19:19 +0200

    give GainProcessor its gain and targetGain members

--- PossGain/Source/Processing/GainProcessor.cpp
+++ PossGain/Source/Processing/GainProcessor.cpp
@@ -1,9 +1,22 @@
 #include "GainProcessor.hpp"
 
+#include <cassert>
+
 void GainProcessor::processBlock(Poss::Buffer& buffer) {
     (void)buffer;
 }
 
+void GainProcessor::setTargetGain(float target) {
+    this->targetGain = target;
+}
+
+float GainProcessor::getCurrentGain() const {
+    return this->gain;
+}
+
 void GainProcessor::prepareToPlay(double /*sampleRate*/,
-                                  int /*maximumExpectedSamplesPerBlock*/) {}
+                                  int /*maximumExpectedSamplesPerBlock*/) {
+    assert(GainProcessor::smoothingForwardWeight > 0.0f &&
+           GainProcessor::smoothingForwardWeight < 1.0f);
+}
 void GainProcessor::releaseResources() {}
--- PossGain/Source/Processing/GainProcessor.hpp
+++ PossGain/Source/Processing/GainProcessor.hpp
@@ -4,9 +4,19 @@
 
 class GainProcessor final : public Poss::Processor {
    public:
+    GainProcessor() = default;
+    ~GainProcessor() = default;
     virtual void processBlock(Poss::Buffer&) override;
 
     virtual void prepareToPlay(double sampleRate,
                                int maximumExpectedSamplesPerBlock) override;
     virtual void releaseResources() override;
+
+    void setTargetGain(float);
+    float getCurrentGain() const;
+
+   private:
+    float targetGain{};
+    float gain{};
+    static constexpr float smoothingForwardWeight = 0.05f;
 };
