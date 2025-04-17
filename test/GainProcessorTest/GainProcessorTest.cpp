d293520b15b0a6ee5e7c4ac83def2c983b58a5c4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-17 20:13:23 +0200

    format

--- test/GainProcessorTest/GainProcessorTest.cpp
+++ test/GainProcessorTest/GainProcessorTest.cpp
@@ -5,19 +5,19 @@
 #include "Processing/GainProcessor.hpp"
 
 TEST(GainProcessor, NewGainProcessorHasZeroGain) {
-    GainProcessor processor {};
+    GainProcessor processor{};
 
     ASSERT_FLOAT_EQ(processor.getCurrentGain(), 0.0);
 }
 
 TEST(GainProcessor, NullsASignalWithin1000Samples) {
-    GainProcessor processor {};
+    GainProcessor processor{};
 
     constexpr std::size_t sz = 1000;
     std::vector left(sz, 1.0f);
     std::vector right(sz, 1.0f);
 
-    Poss::Buffer buffer {&left.front(), &right.front(), sz};
+    Poss::Buffer buffer{&left.front(), &right.front(), sz};
 
     processor.setTargetGain(0.0f);
     processor.processBlock(buffer);
