198691ed190e05add4a095d52b821f0be972433b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-17 20:13:12 +0200

    ensure gain processor set to 0 nulls a signal

--- test/GainProcessorTest/GainProcessorTest.cpp
+++ test/GainProcessorTest/GainProcessorTest.cpp
@@ -1,10 +1,26 @@
 #include "gtest/gtest.h"
 
+#include <vector>
+
 #include "Processing/GainProcessor.hpp"
 
-TEST(GainProcessor, Dummy)
-{
+TEST(GainProcessor, NewGainProcessorHasZeroGain) {
     GainProcessor processor {};
-    (void)processor;
-    SUCCEED();
+
+    ASSERT_FLOAT_EQ(processor.getCurrentGain(), 0.0);
+}
+
+TEST(GainProcessor, NullsASignalWithin1000Samples) {
+    GainProcessor processor {};
+
+    constexpr std::size_t sz = 1000;
+    std::vector left(sz, 1.0f);
+    std::vector right(sz, 1.0f);
+
+    Poss::Buffer buffer {&left.front(), &right.front(), sz};
+
+    processor.setTargetGain(0.0f);
+    processor.processBlock(buffer);
+
+    ASSERT_FLOAT_EQ(processor.getCurrentGain(), 0.0f);
 }
