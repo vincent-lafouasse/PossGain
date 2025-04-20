6f9ebf60da4640a268d8705d28329fccc057d952
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-20 14:58:04 +0200

    sketch tests

--- test/GainProcessorTest/GainProcessorTest.cpp
+++ test/GainProcessorTest/GainProcessorTest.cpp
@@ -4,14 +4,24 @@
 
 #include "Processing/GainProcessor.hpp"
 
+namespace {
+    constexpr float sampleRate = 1e6f; // 1 us per sample
+    constexpr std::size_t nSamplesOneMillisecond = static_cast<std::size_t>(1e-3f * sampleRate);
+    constexpr int ignored = 0;
+}
+
 TEST(GainProcessor, NewGainProcessorHasZeroGain) {
     GainProcessor processor{};
 
     ASSERT_FLOAT_EQ(processor.getCurrentGain(), 0.0);
 }
 
-TEST(GainProcessor, NullsASignalWithin1000Samples) {
+TEST(GainProcessor, ReachesThresholdOnTime) {
     GainProcessor processor{};
+    processor.setTargetGain(0.0f);
+    processor.prepareToPlay(sampleRate, ignored);
+
+    constexpr std::size_t decayMs = 10;
 
     constexpr std::size_t sz = 1000;
     std::vector left(sz, 1.0f);
